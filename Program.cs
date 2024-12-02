6d0bec52c25ddfcccd70f7f77669a06b76f84f73
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-02 20:09:01 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<double, double, double, bool> isTriangle = (a, b, c) =>
+            a + b > c && a + c > b && b + c > a;
+
+        Console.Write("Введіть першу сторону: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть другу сторону: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть третю сторону: ");
+        double c = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(isTriangle(a, b, c) ? "Сторони утворюють трикутник." : "Сторони не утворюють трикутник.");
+    }
+}
